e204d8536b009d19f774b315f1f4156ee8495891
danipoav <dapoav2002@gmail.com>
2025-02-10 00:44:35 +0100

    Creating activity migration

--- database/migrations/2025_02_09_225958_create_activities_table.php
+++ database/migrations/2025_02_09_225958_create_activities_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['surf', 'windsurf', 'kayak', 'atv', 'hot air balloon']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('datetime');
+            $table->boolean('paid')->default(false);
+            $table->text('notes');
+            $table->unsignedTinyInteger('satisfaction')->nullable()->default(null);
             $table->timestamps();
         });
     }
