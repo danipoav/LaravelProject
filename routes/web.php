646d05df0a5937723bb45a9437503e44f2590383
danipoav <dapoav2002@gmail.com>
2025-02-10 00:55:48 +0100

    Creating controller with an index function and making the route o web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('test', ResourceController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
