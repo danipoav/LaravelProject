28e565cd6242f19c943fcfa472d17aa91348f3a7
danipoav <dapoav2002@gmail.com>
2025-02-11 17:48:01 +0100

    Creating activity model fillable and assigment to users to activity

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'user_id',
+        'datetime',
+        'paid',
+        'notes',
+        'satisfaction',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
